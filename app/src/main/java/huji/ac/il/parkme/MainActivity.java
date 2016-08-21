3c494343a45bb91e458d1d9cbca6e1618023a97d
adiefrat <adi.efrat@mail.huji.ac.il>
2016-08-21 17:33:44 +0300

    adi branch1

--- app/src/main/java/huji/ac/il/parkme/MainActivity.java
+++ app/src/main/java/huji/ac/il/parkme/MainActivity.java
@@ -1,54 +1,73 @@
 package huji.ac.il.parkme;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
-    ActionBar actionbar;
+public class MainActivity extends FragmentActivity {
+    TabLayout tabLayout;
     ViewPager viewPager;
-    FragmentPagerAdapter ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.pager);
-        ft = new FragmentPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
 
-        actionbar = getActionBar();
-        // add the adapter to the view pager
-        viewPager.setAdapter(ft);
-        //adding the tabs
-        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionbar.addTab(actionbar.newTab().setText("Personal area").setTabListener(this));
-        actionbar.addTab(actionbar.newTab().setText("Find parking").setTabListener(this));
-        actionbar.addTab(actionbar.newTab().setText("Rent parking").setTabListener(this));
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+        });
     }
 
+    private class CustomAdapter extends FragmentPagerAdapter {
+        private String fragments[] = {"Find parking", "Publish parking"};
+        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
 
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new FindParkingFragment();
+                case 1:
+                    return new PublishParkingFragment();
+                default:
+                    break;
+            }
+            return null;
+        }
 
-    }
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
+        }
     }
 }
--- app/src/main/java/huji/ac/il/parkme/test.java
+++
@@ -1,8 +0,0 @@
-package huji.ac.il.parkme;
-
-/**
- * Created by rotemoh on 16/08/2016.
- */
-public class test {
-    int bla = 03;
-}
