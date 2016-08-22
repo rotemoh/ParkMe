cfe4776005c0a0ce8659e896baa331226dce2432
adiefrat <adi.efrat@mail.huji.ac.il>
2016-08-22 19:50:33 +0300

    updated publish me and find me and splash screen

---
+++ app/src/main/java/huji/ac/il/parkme/UpdatePaymentFragment.java
@@ -0,0 +1,21 @@
+package huji.ac.il.parkme;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adi on 22/08/2016.
+ */
+public class UpdatePaymentFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.update_payment_layout, container, false)
+        return view;
+    }
+}
