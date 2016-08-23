7df4abc0f3c0bc83300bb1393e7db6f941e7d193
adiefrat <adi.efrat@mail.huji.ac.il>
2016-08-23 16:57:55 +0300

    updated publish me and find me and splash screen

--- app/src/main/java/huji/ac/il/parkme/MainActivity.java
+++ app/src/main/java/huji/ac/il/parkme/MainActivity.java
@@ -1,28 +1,43 @@
 package huji.ac.il.parkme;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
-//import com.firebase.client.Firebase;
+import com.firebase.client.Firebase;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Firebase.setAndroidContext(this);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        setting the navigation drawer
+
+
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
+
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -34,16 +49,31 @@ public class MainActivity extends FragmentActivity {
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
             }
         });
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        setSupportActionBar(toolbar);
+        actionBarDrawerToggle.syncState();
     }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
     private class CustomAdapter extends FragmentPagerAdapter {
         private String fragments[] = {"Find parking", "Publish parking", "My calender"};
@@ -74,7 +104,5 @@ public class MainActivity extends FragmentActivity {
         public CharSequence getPageTitle(int position) {
             return fragments[position];
         }
-
-
     }
 }
