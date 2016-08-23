bde248ec53f26bbd4cb5c045755a9967ed7d4439
rotemoh <rotem.vay@gmail.com>
2016-08-23 11:28:53 +0300

    add file google services + lines for firebase in build.gradle

--- app/src/main/java/huji/ac/il/parkme/UpdatePaymentFragment.java
+++ app/src/main/java/huji/ac/il/parkme/UpdatePaymentFragment.java
@@ -15,7 +15,7 @@ public class UpdatePaymentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.update_payment_layout, container, false)
+        view = inflater.inflate(R.layout.update_payment_layout, container, false);
         return view;
     }
 }
