4378572d4b5ae165ab614522cdcdf8fd1742388a
adiefrat <adi.efrat@mail.huji.ac.il>
2016-09-06 12:46:58 +0300

    drawer menu connected2

--- app/src/main/java/huji/ac/il/parkme/CalenderFragment.java
+++ app/src/main/java/huji/ac/il/parkme/CalenderFragment.java
@@ -14,7 +14,21 @@ public class CalenderFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calander_layout, container, false);
+        View rootView = inflater.inflate(R.layout.calander_layout, container, false);
+//        DatePicker simpleDatePicker = (DatePicker)rootView.findViewById(R.id.datePicker); // initiate a date picker
+//
+//        simpleDatePicker.setSpinnersShown(false);
+//        int day = simpleDatePicker.getDayOfMonth(); // get the selected day of the month
+//        int month = simpleDatePicker.getMonth(); // get the selected month
+//        int year = simpleDatePicker.getYear(); // get the selected year
+//        Date minDate = new Date(year-1, month, day);
+//        long minDateMilliSeconds = minDate.getTime();
+//        Date maxDate = new Date(year+5, month, day);
+//        long maxDateMilliSeconds = maxDate.getTime();
+//        simpleDatePicker.setMinDate(minDateMilliSeconds);
+//        simpleDatePicker.setMaxDate(maxDateMilliSeconds);
+
+        return rootView;
 
     }
 }
--- app/src/main/java/huji/ac/il/parkme/PublishParkingFragment.java
+++ app/src/main/java/huji/ac/il/parkme/PublishParkingFragment.java
@@ -5,13 +5,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 /**
  * Created by Adi on 21/08/2016.
  */
 public class PublishParkingFragment extends Fragment {
+    NumberPicker numberpicker;
+    Button resetBtn, updateBtn;
+    EditText addressIn, commentsIn, sDateIn, eDateIn;
+    CheckBox approve, save;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.publish_park_layout, container, false);
-    }
+        View rootView = inflater.inflate(R.layout.publish_park_layout, container, false);
+        addressIn = (EditText)rootView.findViewById(R.id.address_input);
+        commentsIn = (EditText)rootView.findViewById(R.id.comments_input);
+        sDateIn = (EditText)rootView.findViewById(R.id.publish_start_date_input);
+        eDateIn = (EditText)rootView.findViewById(R.id.publish_end_date_input);
+//.setError
+        approve = (CheckBox)rootView.findViewById(R.id.approve_check);
+        save = (CheckBox)rootView.findViewById(R.id.save_details_check);
 
+        numberpicker = (NumberPicker)rootView.findViewById(R.id.cost_picker);
+        numberpicker.setMinValue(0);
+        numberpicker.setMaxValue(100);
+
+        numberpicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                //TODO
+            }
+        });
+        resetBtn = (Button)rootView.findViewById(R.id.reset_btn);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addressIn.setText("");
+                commentsIn.setText("");
+                sDateIn.setText("");
+                eDateIn.setText("");
+
+                approve.setChecked(false);
+                save.setChecked(true);
+
+                numberpicker.setValue(0);
+            }
+        });
+
+        updateBtn = (Button)rootView.findViewById(R.id.update_btn);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Data updated successfully",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
 }
