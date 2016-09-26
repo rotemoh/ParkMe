0e674212b3d97d1b13ff6a673540915367dfbc2e
adiefrat <adi.efrat@mail.huji.ac.il>
2016-09-26 16:37:21 +0300

    with square

--- app/src/main/java/huji/ac/il/parkme/CalenderFragment.java
+++ app/src/main/java/huji/ac/il/parkme/CalenderFragment.java
@@ -16,12 +16,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vdesmet.lib.calendar.MultiCalendarView;
 import com.vdesmet.lib.calendar.OnDayClickListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Adi on 22/08/2016.
@@ -31,31 +34,55 @@ public class CalenderFragment extends Fragment implements OnDayClickListener {
     private Typeface mSelectedTypeface;
     private MultiCalendarView multiMonth;
     private ListView publishLV, orderedLV;
-    private ArrayAdapter<String> adapter;
     //todo: change to the dates of the user
-    private long[] orders, rents;
+    private ArrayList<Long> orders = new ArrayList<>(), rents = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.calander_layout, container, false);
-// Retrieve the CalendarView
+        // Retrieve the CalendarView
         multiMonth = (MultiCalendarView) rootView.findViewById(R.id.multi_calendar);
-//SquaredImageView square = new SquaredImageView(rootView, )
-// Set the first valid day
+        // Set the first valid day
         final Calendar firstValidDay = Calendar.getInstance();
         multiMonth.setFirstValidDay(firstValidDay);
 
-// Set the last valid day
+        // Set the last valid day
         final Calendar lastValidDay = Calendar.getInstance();
         lastValidDay.add(Calendar.YEAR, 1);
         multiMonth.setLastValidDay(lastValidDay);
 
         // Create adapter
-        final CustomDayAdapter adapter = new CustomDayAdapter(orders, rents);
+        CustomDayAdapter adapter = new CustomDayAdapter(orders, rents);
+//        updateCalendar();
 
         // Set listener and adapter
         multiMonth.setOnDayClickListener(this);
         multiMonth.setDayAdapter(adapter);
+
+        Intent intent = getActivity().getIntent();
+        Bundle bundle = intent.getExtras();
+        if(bundle != null) {
+            String startDateStr = bundle.get("startDateStr").toString();
+            Date startDate;
+            String endDateStr = bundle.get("endDateStr").toString();
+            Date endDate;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                startDate = dateFormat.parse(startDateStr);
+                endDate = dateFormat.parse(endDateStr);
+                orders.add(startDate.getTime());
+                orders.add(endDate.getTime());
+                rents.add(startDate.getTime());
+                rents.add(endDate.getTime());
+                //todo-  check (startDate.after(new Date()))
+
+            } catch (Exception e) {
+                Toast.makeText(getActivity(), "Invalid date scheduling",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        multiMonth.notifyDataSetChanged();
+
         return rootView;
     }
     @Override
@@ -76,28 +103,61 @@ public class CalenderFragment extends Fragment implements OnDayClickListener {
         // custom dialog
         final Dialog dialog = new Dialog(getContext());
         dialog.setContentView(R.layout.day_dialog);
-        //        dialog.setTitle("");
-
         dialog.show();
-        ArrayList<String> planetList = new ArrayList<String>();
-        planetList.add("hiiiii");
-        //        publishLV = (ListView)dialog.findViewById(R.id.published_lv);
-        orderedLV = (ListView)dialog.findViewById(R.id.ordered_lv);
-        //        registerForContextMenu(orderedLV);
-        adapter = new ArrayAdapter<String>(getContext(), R.layout.simplerow, R.id.rowTextView, planetList);
-        orderedLV.setAdapter( adapter );
 
-        // Then you can create a listener like so:
-        orderedLV.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        //TODO: change to the addresses in the area. will get it from the DB.
+        final String[] addresses = new String[] {"Android List View",
+                "Gilboa 94 Alfei Menashe",
+                "hi",
+                "Create List View Android"};
+        final String[] info = new String[] {"dis 1, cost 1",
+                "address 2, cost 2",
+                "address 3, cost 3",
+                "address 4, cost 4"};
+        final String[] addresses2 = new String[] {"Android List View",
+                "Gilboa 94 Alfei Menashe",
+                "hi",
+                "Create List View Android"};
+        final String[] info2 = new String[] {"dis 1, cost 1",
+                "address 2, cost 2",
+                "address 3, cost 3",
+                "address 4, cost 4"};
+
+        ArrayAdapter orderedLVAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_2, android.R.id.text1, addresses) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                text1.setText(addresses[position]);
+                text2.setText(info[position]);
+                return view;
+            }
+        };
+
+        ArrayAdapter publishLVAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_2, android.R.id.text1, addresses2) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                text1.setText(addresses2[position]);
+                text2.setText(info2[position]);
+                return view;
+            }
+        };
+        publishLV = (ListView)dialog.findViewById(R.id.published_lv);
+        publishLV.setAdapter(publishLVAdapter);
+        orderedLV = (ListView)dialog.findViewById(R.id.ordered_lv);
+        orderedLV.setAdapter(orderedLVAdapter);
+
+        publishLV.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
                 // 1. Instantiate an AlertDialog.Builder with its constructor
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                //                String dialogTitle = notes.get(position);
-                // 2. Chain together various setter methods to set the dialog characteristics
-                //                builder.setTitle("in this date...");
-
-                //                if (dialogTitle.startsWith("call") || dialogTitle.startsWith("Call")) {
                 //TODO: get the owner number
                 final String numberToCall = "0508655309";
                 builder.setPositiveButton("call owner", new DialogInterface.OnClickListener() {
@@ -110,7 +170,26 @@ public class CalenderFragment extends Fragment implements OnDayClickListener {
                 AlertDialog dialog = builder.create();
                 dialog.show();
                 return true;
-
+            }
+        });
+        // Then you can create a listener like so:
+        orderedLV.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                //TODO: get the owner number
+                final String numberToCall = "0508655309";
+                builder.setPositiveButton("call owner", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intentCall = new Intent(Intent.ACTION_CALL);
+                        intentCall.setData(Uri.parse("tel:" + numberToCall));
+                        startActivity(intentCall);
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
             }
         });
     }
--- app/src/main/java/huji/ac/il/parkme/CustomDayAdapter.java
+++ app/src/main/java/huji/ac/il/parkme/CustomDayAdapter.java
@@ -5,26 +5,28 @@ import android.widget.TextView;
 
 import com.vdesmet.lib.calendar.DayAdapter;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Random;
 
 public class CustomDayAdapter implements DayAdapter {
-    private long[] orders;
-    private long[] rents;
+    private final Random mRandom;
+    private boolean inOrders = false, inRents = false;
+    private ArrayList<Long> order = new ArrayList<>(), rent = new ArrayList<>();
     private static final int[][] CATEGORY_COLORS = {
-            null, null, null,
+            null,
             {Color.parseColor("#A40D44")},
             {Color.parseColor("#81007F")},
             {Color.parseColor("#A40D44"), Color.parseColor("#81007F")}
     };
 
-//    private final Random mRandom;
+    //    private final Random mRandom;
     private final long mToday;
 
-    public CustomDayAdapter(long[] orders, long[] rents) {
-//        mRandom = new Random();
-        orders = orders;
-        rents = rents;
+    public CustomDayAdapter(ArrayList<Long> orders, ArrayList<Long> rents) {
+        mRandom = new Random();
+        order = orders;
+        rent = rents;
         //    Context context = ;
 
         // Get the time in millis of today
@@ -40,18 +42,32 @@ public class CustomDayAdapter implements DayAdapter {
     public int[] getCategoryColors(final long dayInMillis) {
         // Fill the category colors with random int arrays.
 //        final int index = mRandom.nextInt(CATEGORY_COLORS.length);
-        Boolean inOrders = Arrays.asList(orders).contains(dayInMillis),
-                inRents = Arrays.asList(rents).contains(dayInMillis);
+//        Boolean inOrders = Arrays.asList(orders).contains(dayInMillis),
+//                inRents = Arrays.asList(rents).contains(dayInMillis);
+        // Fill the category colors with random int arrays.
+//        final int index = mRandom.nextInt(CATEGORY_COLORS.length);
+//        return CATEGORY_COLORS[index];
+//        rent.add(10000998442L);
+//        order.add(10000998441L);
+//        order.add(dayInMillis);
+        if(rent != null) {
+            inRents = rent.contains(dayInMillis);
+        }
+        if(order != null){
+            inOrders = order.contains(dayInMillis);
+        }
 
-        if(inOrders && inRents){
+        //        System.out.println("dayInMillis " + dayInMillis);
+//        System.out.println("inOrders " + inOrders);
+//        System.out.println("inRents" + inRents);
+        if (inOrders && inRents) {
             return CATEGORY_COLORS[3];
-        }
-        else if (inOrders){
+        } else if (inOrders) {
             return CATEGORY_COLORS[2];
-        }
-        else if (inRents){
+        } else if (inRents) {
             return CATEGORY_COLORS[1];
         }
+
         return CATEGORY_COLORS[0];
     }
 
@@ -81,4 +97,5 @@ public class CustomDayAdapter implements DayAdapter {
                 break;
         }
     }
+
 }
